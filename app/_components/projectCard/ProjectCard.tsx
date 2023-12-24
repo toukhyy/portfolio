3e0b43b5578a6f66e43468e26d604ec75e96c980
toukhyy <tokhy104@gmail.com>
2023-12-24 23:45:18 +0200

    fixed project link

--- app/_components/projectCard/ProjectCard.tsx
+++ app/_components/projectCard/ProjectCard.tsx
@@ -32,13 +32,14 @@ function ProjectCard({ project }: Props) {
         {/* LINKS */}
         <div className="flex w-full border-t-2 border-cream-800">
           {project.liveUrl ? (
-            <Link
+            <a
+              target="_blank"
               className="flex-1 bg-cream-800 py-1 text-center 
         tracking-wider text-orange-100 duration-200 hover:bg-cream-700"
               href={project.liveUrl}
             >
               Live
-            </Link>
+            </a>
           ) : (
             <div className="o relative flex-1 overflow-hidden border-r-2 border-cream-800 py-1 text-center tracking-wider">
               {/* <span>N/A</span> */}
@@ -46,12 +47,13 @@ function ProjectCard({ project }: Props) {
               <div className="absolute -right-1 top-[62%] h-[1.5px] w-40 -rotate-[15deg] bg-cream-800 opacity-50"></div>
             </div>
           )}
-          <Link
+          <a
+            target="_blank"
             className="flex-1 py-1 text-center tracking-wider duration-200 hover:bg-cream-800 hover:text-orange-100"
             href={project.ghRepo}
           >
             Code
-          </Link>
+          </a>
         </div>
       </div>
     </li>
