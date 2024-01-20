f96fa42e99b41babdc9a87ccc17fbc5c434af6b0
toukhyy <tokhy104@gmail.com>
2024-01-20 14:51:45 +0200

    new layout

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,15 +1,16 @@
 import ProjectCard from "@/app/_components/projectCard/ProjectCard";
-import projects from "@/app/_utils/project-data.json";
+import projects from "@/app/_data/projects.json";
 
 function page() {
   return (
-    <section className="relative min-h-screen bg-hero bg-cover px-5 text-cream-1000 lg:px-0">
-      <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col gap-16 pb-16 pt-10">
+    <section className="bg-cover px-5 text-cream-1000 lg:px-0">
+      <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col gap-16 pb-16 pt-12">
         <h1 className="text-3xl font-black uppercase opacity-90 md:text-4xl lg:text-5xl">
           Projects
         </h1>
-        <div className="mx-auto">
-          <ul className="grid auto-rows-fr gap-10  md:grid-cols-2 lg:grid-cols-3">
+        {/* PROJECTS */}
+        <div className="mx-auto w-full">
+          <ul className="grid auto-rows-fr gap-24">
             {projects.map((project) => (
               <ProjectCard key={project.label} project={project} />
             ))}
--- app/_components/projectCard/ProjectCard.tsx
+++ app/_components/projectCard/ProjectCard.tsx
@@ -1,55 +1,73 @@
+import Image from "next/image";
+import { HiMiniArrowUpRight } from "react-icons/hi2";
+import { FaStarOfLife } from "react-icons/fa6";
+import Link from "next/link";
+
 type Props = {
   project: {
     label: string;
     description: string;
+    additionalDesc?: string;
     stack: string[];
     liveUrl?: string;
     ghRepo: string;
+    webImg: string;
+    mobImg?: string;
   };
 };
+const placeholderWeb =
+  "https://res.cloudinary.com/doaw1mqkw/image/upload/v1705397927/estore-web_ejd2ix.png";
+const placeholderMob =
+  "https://res.cloudinary.com/doaw1mqkw/image/upload/v1705684898/projects/estore-mobile_h8ekxn.png";
 
 function ProjectCard({ project }: Props) {
   return (
-    <li>
-      <div className="projectCard-shadow flex h-full w-64 flex-col justify-between overflow-hidden rounded border-2 border-cream-800">
-        <div>
-          <p className="py-2 text-center text-lg font-semibold capitalize">
-            {project.label}
-          </p>
-          <div className="mx-auto h-[1.5px] w-5/6 bg-cream-800"></div>
-        </div>
-        {/* DESC AND STACK */}
-        <div className="mb-2 px-4 py-3.5">
-          <p className="text-center font-semibold">Description</p>
-          <p className="mb-2 text-center text-sm">{project.description}</p>
-          <p className="text-center font-semibold">Stack</p>
-          <p className="text-center text-sm capitalize">
-            {project.stack.join(" • ")}
-          </p>
-        </div>
-        {/* LINKS */}
-        <div className="flex w-full border-t-2 border-cream-800">
-          {project.liveUrl ? (
+    <li className="flex w-full flex-row justify-between gap-10 even:flex-row-reverse">
+      {/* img with background */}
+      <div className="relative flex flex-[1.5] items-center justify-center rounded-md bg-cream-200 p-14">
+        <Image
+          alt={project.label}
+          src={project.webImg || placeholderWeb}
+          width="1150"
+          height="650"
+          className="max-w-xl rounded-md"
+        />
+        {project.mobImg && (
+          <Image
+            alt={project.label}
+            src={project.mobImg}
+            width="260"
+            height="420"
+            className="absolute right-10 top-32 h-56 w-min rounded-md shadow-base"
+          />
+        )}
+      </div>
+      {/* description */}
+      <div className="flex h-full flex-1 flex-col justify-end p-4">
+        <Link
+          target="_blank"
+          href={project.liveUrl || project.ghRepo}
+          className="group relative mb-2.5 w-max"
+        >
+          <p className=" text-3xl font-bold uppercase">{project.label}</p>
+          <HiMiniArrowUpRight className="absolute -right-6 top-0 h-6 w-6 text-cream-600 duration-200 group-hover:-right-7 group-hover:-top-1" />
+        </Link>
+        <p className="mb-3 max-w-sm text-xs text-cream-600">
+          {project.description}
+        </p>
+
+        <div className="flex items-center gap-1 font-thin text-cream-600">
+          <FaStarOfLife className="h-2.5 w-2.5" />
+          <div className="text-xs">
+            <span>click the label for the live version</span>
+            <span> or check the code </span>
             <a
-              target="_blank"
-              className="flex-1 bg-cream-800 py-1 text-center 
-        tracking-wider text-orange-100 duration-200 hover:bg-cream-700"
-              href={project.liveUrl}
+              href={project.ghRepo}
+              className="rounded bg-cream-100 p-0.5 text-cream-600 duration-200 hover:bg-cream-300"
             >
-              Live
+              here.
             </a>
-          ) : (
-            <div className="o relative flex-1 overflow-hidden border-r-2 border-cream-800 py-1 text-center tracking-wider">
-              <span className="font-bold text-cream-800">—</span>
-            </div>
-          )}
-          <a
-            target="_blank"
-            className="flex-1 py-1 text-center tracking-wider duration-200 hover:bg-cream-800 hover:text-orange-100"
-            href={project.ghRepo}
-          >
-            Code
-          </a>
+          </div>
         </div>
       </div>
     </li>
